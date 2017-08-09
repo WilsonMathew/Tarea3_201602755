98e566ae079bc77bba5a5c3f1a19ad477d837870
Majus <Majus@192.168.56.1>
2017-08-09 16:17:02 -0600

    Codigo prueba final

--- src/ipctarea3/Consola.java
+++ src/ipctarea3/Consola.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ipctarea3;
 import java.util.Scanner;
-import java.io.IOException;
+
 
 /**
  *
@@ -13,20 +8,19 @@ import java.io.IOException;
  */
 public class Consola {
     
+    // Opciones para manipular el menu
     private static int opcion;
     private static Scanner sc = new Scanner(System.in);
     
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException, InterruptedException{
+    public static void main(String[] args) {
         // TODO code application logic here
         menuF();
     }
         
-        // Menu Funcional
+    // Menu Funcional
     public static void menuF(){
         
+        // OPcioens para dirigirse al menu Uusuarios y palindromo
         Usuarios us = new Usuarios();
         Palindromo pl = new Palindromo();
         
